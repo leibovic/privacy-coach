d337b2541518854168be5801074e347eca47e3a4
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-09-18 16:05:06 -0700

    Bug 1064947 - Default specific prefs
    restore original user prefs when add-on is disabled/uninstalled
    
    add more prefs
    
    don't throw an excpetion if the crash reporter isn't present

--- bootstrap.js
+++ bootstrap.js
@@ -3,6 +3,47 @@ const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
+XPCOMUtils.defineLazyGetter(this, "CrashReporter", function() {
+  try {
+    return Cc["@mozilla.org/xre/app-info;1"].getService(Ci["nsICrashReporter"]);
+  } catch (e) {
+    // Fennec may not be built with the crash reporter, so just return an empty
+    // object instead of throwing an exception.
+    return {};
+  }
+});
+
+// Prefix for prefs to store original user prefs.
+var PREF_PREFIX = "extensions.fxconfidential.";
+
+// Prefs that the add-on sets.
+var PREFS = [
+  {
+    type: "bool",
+    key: "privacy.donottrackheader.enabled",
+    value: true
+  },
+  {
+    type: "int",
+    key: "privacy.donottrackheader.value",
+    value: 1 // "Do not track me."" This will be deprecated in Fx35 with bug 1042135.
+  },
+  {
+    type: "int",
+    key: "network.cookie.cookieBehavior",
+    value: 1 // "Enabled, excluding 3rd party"
+  },
+  {
+    type: "bool",
+    key: "toolkit.telemetry.enabled",
+    value: false
+  },
+  {
+    key: "datareporting.crashreporter.submitEnabled",
+    value: false
+  }
+];
+
 // An example of how to create a string bundle for localization.
 XPCOMUtils.defineLazyGetter(this, "Strings", function() {
   return Services.strings.createBundle("chrome://fxconfidential/locale/fxconfidential.properties");
@@ -37,35 +78,57 @@ var windowListener = {
 };
 
 function startup(data, reason) {
-  switch(reason) {
-    case ADDON_INSTALL:
-    case ADDON_ENABLE:
-      break;
+  if (reason == ADDON_INSTALL || reason == ADDON_ENABLE) {
+    // Store the original pref values so that we can restore them when the add-on
+    // is uninstalled or disabled.
+    for (let pref of PREFS) {
+      if (pref.key == "datareporting.crashreporter.submitEnabled") {
+        // Crash reporter submit pref must be fetched from nsICrashReporter service.
+        Services.prefs.setBoolPref(PREF_PREFIX + pref.key, CrashReporter.submitReports);
+        CrashReporter.submitReports = pref.value;
+      } else if (pref.type == "bool") {
+        Services.prefs.setBoolPref(PREF_PREFIX + pref.key, Services.prefs.getBoolPref(pref.key));
+        Services.prefs.setBoolPref(pref.key, pref.value);
+      } else if (pref.type == "int") {
+        Services.prefs.setIntPref(PREF_PREFIX + pref.key, Services.prefs.getIntPref(pref.key));
+        Services.prefs.setIntPref(pref.key, pref.value);
+      } else {
+        Cu.reportError("Firefox Confidential: Can't set unknown pref type: " + JSON.stringify(pref));
+      }
+    }
   }
 
-  // Load into any existing windows
+  // Load UI features into the main window.
   let windows = Services.wm.getEnumerator("navigator:browser");
   while (windows.hasMoreElements()) {
     let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
     loadIntoWindow(domWindow);
   }
-
-  // Load into any new windows
   Services.wm.addListener(windowListener);
 }
 
 function shutdown(data, reason) {
+  if (reason == ADDON_UNINSTALL || reason == ADDON_DISABLE) {
+    // Restore the original pref values.
+    for (let pref of PREFS) {
+      if (pref.key == "datareporting.crashreporter.submitEnabled") {
+        // Crash reporter submit pref must be fetched from nsICrashReporter service.
+        CrashReporter.submitReports = Services.prefs.getBoolPref(PREF_PREFIX + pref.key);
+      } else if (pref.type == "bool") {
+        Services.prefs.setBoolPref(pref.key, Services.prefs.getBoolPref(PREF_PREFIX + pref.key));
+      } else if (pref.type == "int") {
+        Services.prefs.setIntPref(pref.key, Services.prefs.getIntPref(PREF_PREFIX + pref.key));
+      } else {
+        Cu.reportError("Firefox Confidential: Can't reset unknown pref type: " + JSON.stringify(pref));
+      }
 
-  switch(reason) {
-    case ADDON_UNINSTALL:
-    case ADDON_DISABLE:
-      break;
+      // Clear the pref that we set for restore purposes.
+      Services.prefs.clearUserPref(PREF_PREFIX + pref.key)
+    }
   }
 
-  // Stop listening for new windows
+  // Unload UI features from the main window.
   Services.wm.removeListener(windowListener);
-
-  // Unload from any existing windows
   let windows = Services.wm.getEnumerator("navigator:browser");
   while (windows.hasMoreElements()) {
     let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
