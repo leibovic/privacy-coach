2001c0a68ffc76384c7c0b19e9781762a37ae5b1
Richard Newman <rnewman@twinql.com>
2014-10-28 12:47:30 -0700

    Split build script into separate build/deploy.

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source $(dirname $0)/build
+
+# Replace this value to push to different release channels.
+# Nightly = org.mozilla.fennec
+# Aurora = org.mozilla.fennec_aurora
+# Beta = org.mozilla.firefox_beta
+# Release = org.mozilla.firefox
+ANDROID_APP_ID=org.mozilla.firefox
+
+# Push the add-on to your device to test.
+adb push "$XPI" /sdcard/"$XPI" && \
+adb shell am start -a android.intent.action.VIEW \
+                   -c android.intent.category.DEFAULT \
+                   -d file:///mnt/sdcard/"$XPI" \
+                   -n $ANDROID_APP_ID/.App && \
+echo Pushed $XPI to $ANDROID_APP_ID
