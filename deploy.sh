b206979167520288e6655d224d50c2a2d39766d1
Margaret Leibovic <margaret.leibovic@gmail.com>
2016-02-22 16:42:16 -0800

    Update build scripts

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,15 @@ source $(dirname $0)/build
 # Release = org.mozilla.firefox
 ANDROID_APP_ID=org.mozilla.fennec
 
-# Push the add-on to your device to test.
+
+# Push the add-on to your device to test
 adb push "$XPI" /sdcard/"$XPI" && \
+
+# Push an HTML page to link to add-on
+adb push install.html /sdcard/install.html && \
+
 adb shell am start -a android.intent.action.VIEW \
                    -c android.intent.category.DEFAULT \
-                   -d file:///mnt/sdcard/"$XPI" \
-                   -n $ANDROID_APP_ID/.App && \
+                   -d file:///mnt/sdcard/install.html \
+                   -n $ANDROID_APP_ID/org.mozilla.gecko.BrowserApp && \
 echo Pushed $XPI to $ANDROID_APP_ID
